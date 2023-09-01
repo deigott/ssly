f6ab5e17fa17412c56b6ddd5d0e2da81471471a0
Dei Gott <dollyetal@proton.me>
2023-09-01 11:37:31 +0100

    Adding main.

---
+++ srcs/main.c
@@ -0,0 +1,33 @@
+#include "ssly.h"
+
+t_ssly  *g_ssly;
+
+/**
+ * @brief Initialize Ping Structure
+ * Allocates memory for the global ping structure and initializes its fields.
+ * The structure holds information related to ping operations, including IP address,
+ * arguments, loop control, ICMP header, and destination address.
+ *
+ * @param None.
+ * @return None.
+ */
+void    init_struct()
+{
+	g_ssly = (t_ssly *)malloc(sizeof(t_ssly));
+	if (g_ssly == NULL)
+        show_errors("ssly: can't allocate memory\n", EX_OSERR);
+    g_ssly->args = NULL;
+	return ;
+}
+
+
+int     main(int argc, char **argv)
+{
+    init_struct();
+    /* TO-DO : Parsing command line argument */
+    parse_clo(argc, argv);
+    show_usage();
+    show_help();
+ 
+    return (0);
+}
