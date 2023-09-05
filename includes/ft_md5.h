2f0f20e397421637076ec07952bf3902fb47f77a
Dei Gott <dollyetal@proton.me>
2023-09-05 08:07:06 +0100

    Md5 algorithm header file.

--- includes/ft_md5.h
+++ includes/ft_md5.h
@@ -22,28 +22,32 @@ typedef unsigned short int UINT2;
 /* UINT4 defines a four byte word */
 typedef unsigned long int UINT4;
 
+/*
+ * Constants defined by the MD5 algorithm
+ */
+#define A 0x67452301
+#define B 0xefcdab89
+#define C 0x98badcfe
+#define D 0x10325476
+
+
 
 /* ROTATE_LEFT rotates x left n bits.
  */
 #define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
 
+typedef struct    s_md5_ctx
+{
+    uint64_t size;        // Size of input in bytes
+    uint32_t buffer[4];   // Current accumulation of hash
+    uint8_t input[64];    // Input to be used in the next step
+    uint8_t digest[16];   // Result of algorithm
+}                 t_md5_ctx;
 
-
-
- typedef struct s_md5_ctx
- {
-    UINT4 A;
-    UINT4 B;
-    UINT4 C;
-    UINT4 D;
-    UINT4 count[2];        /* number of bits, modulo 2^64 (lsb first) */
-    unsigned char buffer[64];
- }              t_md5_ctx;
-
-
-void md5_init(void);
-void md5_update(void);
-void md5_final(void);
-void md5( void );
+void  md5_init(void);
+void  md5_update(uint8_t *input, size_t input_len);
+void  md5_algo(uint32_t *input);
+void  md5_final(void);
+void  md5( void );
 
 #endif
