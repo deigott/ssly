ffc97249e3cd158886d149277f7065bab2cf07d5
Dei Gott <dollyetal@proton.me>
2023-09-06 11:00:36 +0100

    Adding memset function.

--- includes/ft_libft.h
+++ includes/ft_libft.h
@@ -17,4 +17,6 @@ void    ft_putstr(char **string);
 char	*ft_strdup(const char *s1);
 char    *get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2, size_t size);
+void	*ft_memset(void *b, int c, size_t len);
+
 #endif
