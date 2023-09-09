142100a45da2cca130248a644424eb852b81f64b
Dei Gott <dollyetal@proton.me>
2023-09-09 18:19:54 +0100

    Adding include of whirlpool header and struct

--- includes/ssly.h
+++ includes/ssly.h
@@ -7,6 +7,7 @@
 # include "ft_libft.h"
 # include "ft_md5.h"
 # include "ft_sha256.h"
+# include "ft_whirlpool.h"
 # include "ft_utils.h"
 # include <sysexits.h>
 # include <fcntl.h>
@@ -18,6 +19,7 @@
 # define OPT_QUIET    (1 << 4)
 # define OPT_MD5	  (1 << 5)
 # define OPT_SHA2     (1 << 6)
+# define OPT_WHIRL    (1 << 7)
 
 typedef struct  s_args
 {
@@ -25,6 +27,7 @@ typedef struct  s_args
 	char	**files;
 	int		options;
 	char	*string;
+    int     flag_stdin;
 }               t_args;
 
 
@@ -33,14 +36,17 @@ typedef struct  s_ssly
     t_args      		*args;
 	t_md5_ctx			*md5_ctx;
 	t_sha256_ctx		*sha256_ctx;
+    t_whirlpool_ctx     *whirlpool_ctx;
 }               t_ssly;
 
+
+typedef void (*HashFunction)();
 
 /* Global Variable */
 extern t_ssly   *g_ssly;
 
 
 void    parse_clo(int len, char *clo_args[]);
-void    sha2();
+
 
 #endif
